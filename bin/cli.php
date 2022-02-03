d06be25bfa3b6f1cafa9f40b2d63c3f4e76b029f
Зинченко Данила <zinchenko@effective-group.ru>
2022-02-03 10:07:23 +0300

    перевод консольного приложения на di контейнер symfony и раскоментирование своего di контейнера для unit-тестов

--- bin/cli.php
+++ bin/cli.php
@@ -1,19 +1,23 @@
 #!C:\php php
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
 use DD\ContactList\Infrastructure\Console\AppConsole;
 use DD\ContactList\Infrastructure\Console\Output\OutputInterface;
 use DD\ContactList\Infrastructure\Di\Container;
 use DD\ContactList\Infrastructure\Di\ContainerInterface;
-
-require_once __DIR__ . '/../src/Infrastructure/Autoloader/Autoloader.php';
+use DD\ContactList\Infrastructure\DI\SymfonyDiContainerInit;
 
 (new AppConsole(
     require __DIR__ . '/../config/console.handlers.php',
     static function (ContainerInterface $di): OutputInterface {
         return $di->get(OutputInterface::class);
     },
-    static function (): ContainerInterface {
-        return Container::createFromArray(require __DIR__ . '/../config/dev/di.php');
-    }
+    new SymfonyDiContainerInit(
+        __DIR__ . '/../config/dev/di.xml',
+        [
+            'kernel.project_dir' => __DIR__ . '/../'
+        ]
+    )
 ))->dispatch();

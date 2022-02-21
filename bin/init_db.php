acaf2130457f397b018156e6a80a4c02c6ffae7c
Зинченко Данила <zinchenko@effective-group.ru>
2022-02-21 23:37:39 +0300

    импорт данных в бд из json

---
+++ bin/init_db.php
@@ -0,0 +1,103 @@
+#!C:\php php
+<?php
+
+$dsn = "pgsql:host=localhost;port=5432;dbname=contact_list_db";
+$dbConnection = new PDO($dsn, 'postgres', 'qwerty12');
+
+/**
+ * Импорт данных о пользователе
+ */
+$dbConnection->query('DELETE FROM users');
+
+$userData = json_decode(file_get_contents(__DIR__ . '/../data/users.json'), true, 512, JSON_THROW_ON_ERROR);
+foreach ($userData as $userItem) {
+    $sql = "INSERT INTO users(id, login, password) VALUES ({$userItem['id']}, '{$userItem['login']}', '{$userItem['password']}')";
+    $dbConnection->query($sql);
+}
+
+//$userFromDb = $dbConnection->query('SELECT * FROM users')->fetchAll(PDO::FETCH_ASSOC);
+
+/**
+ * Импорт данных о контактах
+ */
+$dbConnection->query('DELETE FROM contacts');
+
+$contactsJsonData = [
+    'recipients' => json_decode(file_get_contents(__DIR__ . '/../data/recipient.json'), true, 512, JSON_THROW_ON_ERROR),
+    'kinsfolk' => json_decode(file_get_contents(__DIR__ . '/../data/kinsfolk.json'), true, 512, JSON_THROW_ON_ERROR),
+    'customers' => json_decode(file_get_contents(__DIR__ . '/../data/customers.json'), true, 512, JSON_THROW_ON_ERROR),
+    'colleagues' => json_decode(file_get_contents(__DIR__ . '/../data/colleagues.json'), true, 512, JSON_THROW_ON_ERROR)
+];
+
+foreach ($contactsJsonData as $type => $contactsCollection) {
+    foreach ($contactsCollection as $contact) {
+        $status = $contact['status'] ?? 'null';
+        $ringtone = $contact['ringtone'] ?? 'null';
+        $hotkey = $contact['hotkey'] ?? 'null';
+
+        $contractNumber = $contact['contract_number'] ?? 'null';
+        $averageTransactionAmount = $contact['average_transaction_amount'] ?? 'null';
+        $discount = $contact['discount'] ?? 'null';
+        $timeToCall = $contact['time_to_call'] ?? 'null';
+
+        $department = $contact['department'] ?? 'null';
+        $position = $contact['position'] ?? 'null';
+        $roomNumber = $contact['room_number'] ?? 'null';
+
+        $sql = <<<EOF
+INSERT INTO contacts (id_recipient, full_name, birthday, profession, status, ringtone, hotkey, contract_number, average_transaction_amount, discount, time_to_call, department, position, room_number, type)
+VALUES
+(
+ {$contact['id_recipient']},
+ '{$contact['full_name']}',
+ '{$contact['birthday']}',
+ '{$contact['profession']}',
+ '{$status}',
+ '{$ringtone}',
+ '{$hotkey}',
+ {$contractNumber},
+ {$averageTransactionAmount},
+ '{$discount}',
+ '{$timeToCall}',
+ '{$department}',
+ '{$position}',
+ {$roomNumber},
+ '{$type}'
+);
+EOF;
+        $dbConnection->query($sql);
+    }
+}
+
+/**
+ * Импорт адресов контактов
+ */
+$dbConnection->query('DELETE FROM address');
+
+$addressJsonData = json_decode(file_get_contents(__DIR__ . '/../data/address.json'), true, 512, JSON_THROW_ON_ERROR);
+foreach ($addressJsonData as $addressItem) {
+    $sql = "INSERT INTO address(id, id_recipient, address, status) VALUES ({$addressItem['id_address']}, {$addressItem['id_recipient']}, '{$addressItem['address']}', '{$addressItem['status']}')";
+    $dbConnection->query($sql);
+}
+
+/**
+ * Импорт черного списка
+ */
+$dbConnection->query('DELETE FROM contact_list');
+
+$contactListJsonData = json_decode(file_get_contents(__DIR__ . '/../data/contact_list.json'), true, 512, JSON_THROW_ON_ERROR);
+foreach ($contactListJsonData as $contactListItem) {
+    $sql = "INSERT INTO contact_list(id, id_recipient, blacklist) VALUES ({$contactListItem['id_entry']}, {$contactListItem['id_recipient']}, '{$contactListItem['blacklist']}')";
+    $dbConnection->query($sql);
+}
+
+/**
+ * Импорт телефонной книги
+ */
+$dbConnection->query('DELETE FROM phone_number');
+
+$phoneNumberJsonData = json_decode(file_get_contents(__DIR__ . '/../data/phone_number.json'), true, 512, JSON_THROW_ON_ERROR);
+foreach ($phoneNumberJsonData as $phoneNumberItem) {
+    $sql = "INSERT INTO phone_number(id_phone_number, id_recipient, phone_number, operator) VALUES ({$phoneNumberItem['id_phone_number']}, {$phoneNumberItem['id_recipient']}, '{$phoneNumberItem['phone_number']}', '{$phoneNumberItem['operator']}')";
+    $dbConnection->query($sql);
+}
---
+++ data/dump/contact_list_db.sql
@@ -0,0 +1,145 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.2
+-- Dumped by pg_dump version 14.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: address; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.address (
+    id integer,
+    id_recipient integer,
+    address character varying(255),
+    status character varying(4)
+);
+
+
+ALTER TABLE public.address OWNER TO postgres;
+
+--
+-- Name: contact_list; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.contact_list (
+    id integer,
+    id_recipient integer,
+    blacklist boolean
+);
+
+
+ALTER TABLE public.contact_list OWNER TO postgres;
+
+--
+-- Name: contacts; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.contacts (
+    id_recipient integer,
+    full_name character varying(255),
+    birthday date,
+    profession character varying(100),
+    status character varying(30),
+    ringtone character varying(30),
+    hotkey character varying(5),
+    contract_number integer,
+    average_transaction_amount integer,
+    discount character varying(5),
+    time_to_call character varying(35),
+    department character varying(40),
+    "position" character varying(40),
+    room_number integer,
+    type character varying(15)
+);
+
+
+ALTER TABLE public.contacts OWNER TO postgres;
+
+--
+-- Name: phone_number; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.phone_number (
+    id_phone_number integer,
+    id_recipient integer,
+    phone_number character varying(12),
+    operator character varying(20)
+);
+
+
+ALTER TABLE public.phone_number OWNER TO postgres;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    id integer,
+    login character varying(50),
+    password character varying(60)
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- Data for Name: address; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.address (id, id_recipient, address, status) FROM stdin;
+\.
+
+
+--
+-- Data for Name: contact_list; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.contact_list (id, id_recipient, blacklist) FROM stdin;
+\.
+
+
+--
+-- Data for Name: contacts; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.contacts (id_recipient, full_name, birthday, profession, status, ringtone, hotkey, contract_number, average_transaction_amount, discount, time_to_call, department, "position", room_number, type) FROM stdin;
+\.
+
+
+--
+-- Data for Name: phone_number; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.phone_number (id_phone_number, id_recipient, phone_number, operator) FROM stdin;
+\.
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (id, login, password) FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
